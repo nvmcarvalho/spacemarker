58c06ae063be70a43e8377b4b3cfa3f2d6fab2f5
Joao Vitor tbrl <joaovcds0902@gmail.com>
2023-06-27 14:46:44 -0300

    adicionada função que abre a caixa de dialogo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 import winsound
 import sys
+from tkinter import Tk, simpledialog
+import json
+pygame.init()
 tamanho = 800, 600
 tela = pygame.display.set_mode(tamanho)
 icone = pygame.image.load("space.png")
@@ -12,5 +15,16 @@ preto = (0, 0, 0)
 branco = (255, 255, 255)
 vermelho = (255, 0, 0)
 fonte = pygame.font.Font(None, 24)
+
+def abrirDialogo():
+    root = Tk()
+    root.withdraw()
+    try:
+        nome = simpledialog.askstring("Estrela", "Digite o nome da estrela:")
+    except:
+        nome = None
+    root.destroy()
+    return nome
+
 
 pygame.display.flip()
